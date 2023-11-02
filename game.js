412b96c49e587fab8a43ee18d7e6cacf9fa86bb1
Kshitij Kotecha <kshitijkotecha@Kshitijs-MacBook-Air.local>
2023-11-02 18:27:23 +0530

    done

--- game.js
+++ game.js
@@ -1,5 +1,81 @@
 // Iteration 2: Generate 2 random number and display it on the screen
 
+let number1 = document.getElementById("number1")
+let number2 = document.getElementById("number2")
+
+function generateRandomNumber(){
+    let num1 = Math.round(Math.random()*100)
+    let num2 = Math.round(Math.random()*100)
+    
+    number1 .innerHTML = num1
+    number2.innerHTML = num2
+}
+generateRandomNumber()
+
 // Iteration 3: Make the options button functional
 
+let score = 0;
+const greaterThan = document.getElementById("greater-than")
+const equalTo = document.getElementById("equal-to")
+const lesserThan = document.getElementById("lesser-than")
+
+greaterThan.addEventListener("click" , () =>{
+    if (number1.innerHTML > number2.innerHTML){
+        score ++;
+        resetTimer(timerId)
+    }else{
+        location.href = "./gameover.html"
+    }
+    generateRandomNumber()
+})
+
+equalTo.addEventListener("click" , () =>{
+    if (number1.innerHTML === number2.innerHTML){
+        score ++
+    }else{
+        location.href = "./gameover.html"
+    }
+    generateRandomNumber()
+    resetTimer(timerId)
+})
+
+lesserThan.addEventListener("click" , () =>{
+    if (number1.innerHTML < number2.innerHTML){
+        score ++
+    }else{
+        location.href = "./gameover.html"
+    }
+    generateRandomNumber()
+    resetTimer(timerId)
+})
+
 // Iteration 4: Build a timer for the game
+
+const timer = document.getElementById("timer")
+var timerId;
+var time = 5;
+
+function startTimer(){
+    let time = 5;
+    timer.innerHTML = time;
+    timerId = setInterval(() => {
+        time--
+        if(time == 0){
+            window.location.href ="./gameover.html";
+        }
+        timer.innerHTML = time;
+    }, 1000);
+
+// store the score 
+localStorage.setItem("squad56",score);
+
+}
+
+function resetTimer(hello){
+    clearInterval(hello)  // we are stopping the interval loop
+    startTimer()
+
+}
+
+startTimer()
+
--- score.js
+++ score.js
@@ -1,1 +1,16 @@
 // Iteration 5: Store the player score and display it on the game over screen
+
+const scoreBox = document.getElementById("score-box");
+
+//we will get the score from our vault (local storage)
+let passcode = localStorage.getItem("squad56");
+console.log(passcode)
+
+scoreBox.innerHTML = passcode;
+
+const playAgainButton = document.getElementById("play-again-button")
+
+playAgainButton.onclick =() => {
+    location.href = "./game.html"
+}
+
